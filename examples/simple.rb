d7bad49df577275d9ea426aebd76bf4dec422099
Ben! <ben@bensites.com>
2016-03-13 14:00:31 -0500

    Used require in example instead of relative.

--- examples/simple.rb
+++ examples/simple.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'awesome_print'
-require_relative '../lib/wit_bot'
+require 'wit_bot'
 
 WitBot.configure do |c|
   c.token = ENV['WIT_ACCESS_TOKEN'] # Create a wit bot and set the token here
