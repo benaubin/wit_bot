236a8cd314f2e5eae8a0c4e5683e3378ce7ac084
Ben! <ben@bensites.com>
2016-03-13 17:10:12 -0500

    better request processing (supports arrays)

--- lib/wit_bot/requests/wit_request.rb
+++ lib/wit_bot/requests/wit_request.rb
@@ -6,10 +6,21 @@ module WitBot
 
     def request(request=http)
       if request.code.between? 200, 299
-        request.parse.with_indifferent_access
+        process_response request.parse
       else
         response = request.parse.with_indifferent_access
         raise WitError.new(response[:status]), (response[:error] || response[:body])
+      end
+    end
+
+    private
+    def process_response(response)
+      if response.respond_to? :with_indifferent_access
+        response.with_indifferent_access
+      elsif response.respond_to? :map
+        response.map {|r| process_response r}
+      else
+        response
       end
     end
   end
