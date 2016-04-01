1955e4dec4737ea09dc021b054d9c3839108adeb
Ben <ben@bensites.com>
2016-04-01 10:44:30 -0500

    Configuration should be in module WitBot

--- lib/wit_bot/configuration.rb
+++ lib/wit_bot/configuration.rb
@@ -1,9 +1,11 @@
-class Configuration
-  attr_accessor :token, :host, :minimum_confidence, :version
-  def initialize
-    @token = nil
-    @host = 'https://api.wit.ai'
-    @minimum_confidence = 0
-    @version = "20160313"
+module WitBot
+  class Configuration
+    attr_accessor :token, :host, :minimum_confidence, :version
+    def initialize
+      @token = nil
+      @host = 'https://api.wit.ai'
+      @minimum_confidence = 0
+      @version = '20160313'
+    end
   end
 end
