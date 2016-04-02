240946287bc2c14b99d8d7d5ee7d2e0bf70551f4
Ben <ben@bensites.com>
2016-04-02 10:23:42 -0500

    Better requiring for examples and repl

--- examples/helpers/helpers.rb
+++ examples/helpers/helpers.rb
@@ -1,4 +1,7 @@
-require_relative '../../lib/wit_bot'
+lib = File.expand_path('../../../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+require 'wit_bot'
 require 'ap'
 
 WitBot.configure do |c|
