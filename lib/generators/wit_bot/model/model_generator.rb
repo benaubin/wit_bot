5fff0e50a99ac8d2f78ebe1f35bd0c9a529d19b2
Ben <ben@bensites.com>
2016-04-03 13:17:16 -0500

    Allow --force through the model generator

--- lib/generators/wit_bot/model/model_generator.rb
+++ lib/generators/wit_bot/model/model_generator.rb
@@ -3,11 +3,15 @@ module WitBot
     class ModelGenerator < Rails::Generators::Base
       source_root File.expand_path('../templates', __FILE__)
       argument :user_model, type: :string, default: 'User'
+      hook_for :force, type: :boolean
 
       def create_conversations
         user_lower = user_model.underscore
 
-        generate 'migration', 'CreateConversations', 'object:text'
+        migration_options = 'migration', 'CreateConversations', 'object:text', "#{user_lower}:references"
+        migration_options << '--force' if options[:force]
+
+        generate *migration_options
         template 'model.rb.erb', 'app/models/conversation.rb'
 
         user_file = "app/models/#{user_lower}.rb"
