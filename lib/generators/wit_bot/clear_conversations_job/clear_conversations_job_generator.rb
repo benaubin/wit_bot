c2edf982a6668517d5c21b7a41e48d81b46fb815
Ben <ben@bensites.com>
2016-04-04 08:06:55 -0500

    fix another problem with the job generator

--- lib/generators/wit_bot/clear_conversations_job/clear_conversations_job_generator.rb
+++ lib/generators/wit_bot/clear_conversations_job/clear_conversations_job_generator.rb
@@ -7,7 +7,7 @@ module WitBot
         gem 'clockwork', '~> 1.2.0'
 
         template 'clock.rb', 'clock.rb'
-        template 'clear_conversations_job.rb', 'app/jobs/clear_conversations_job.rb'
+        template 'job.rb', 'app/jobs/clear_conversations_job.rb'
       end
     end
   end
